7b493ed6a065a51d6b51e13dd11674a3182267e8
Patrick <ptklorran@gmail.com>
2023-05-12 00:18:00 -0300

    updates

--- src/app/useCases/5#Ocorrencias/admin/list.js
+++ src/app/useCases/5#Ocorrencias/admin/list.js
@@ -11,12 +11,12 @@ module.exports = async (req, res) => {
         if(req.body.all) {
             options.pagination = false
         } else {
-            const queryId = req.body.id_ocorrencia ? { regex: { $regex: new RegExp(parseInt(req.body.id_ocorrencia)), $options: 'i'}} : false
-            // const queryNome = req.body.nome ? { regex: { $regex: new RegExp(req.body.nome), $options: 'i'}} : false
+            const queryId = req.body.id_ocorrencia ? { regex: { $regex: new RegExp(req.body.id_ocorrencia), $options: 'i'}} : false
+            const queryNome = req.body.nome ? { regex: { $regex: new RegExp(req.body.nome), $options: 'i'}} : false
             const queryBase = { regex: { $regex: new RegExp(req.body.base), $options: 'i'}}
             queryBase ? query.base = queryBase.regex : false
-            // queryNome ? query.nome = queryNome.regex : false
-            queryId ? query.id_ocorrencia = queryNome.regex : false
+            queryNome ? query.nome = queryNome.regex : false
+            queryId ? query.id_ocorrencia = queryId.regex : false
         }
 
         const paginate = await Model.paginate(
@@ -24,7 +24,6 @@ module.exports = async (req, res) => {
             {
                 ...options,
                 lean: true,
-                leanWithId: true,
                 page: parseInt(req.body.page),
                 limit: req.body.limitPerPage ? req.body.limitPerPage : 2000
             })
